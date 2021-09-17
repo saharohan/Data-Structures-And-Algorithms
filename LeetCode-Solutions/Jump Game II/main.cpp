a364b9683b62d0ea7cbe279e36da38355905e09d
Rohan Saha <36517636+saharohan@users.noreply.github.com>
2021-09-17 22:32:55 +0530

    Create main.cpp

---
+++ LeetCode-Solutions/Jump Game II/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+       int j = 0;
+       int m =0;
+        int c=0;
+        
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(nums[i]+i > m)
+            {
+                m = nums[i]+i;
+            }
+            
+            if(c == i)
+            {
+                c= m;
+                j++;
+            }
+        }
+        return j;
+    }
+        
+            
+};
